6319f9ac9f1763797427e446c47da4651ee07aa5
kimtaesu <kimtaesoo188@gmail.com>
2019-07-29 13:19:56 +0900

    CGFloat

---
+++ CGFloat+Advance.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+let value: CGFloat = 10
+
+print("advance: \(value.advanced(by: 1))")
+// 10 + 1 = 11
+
+print("distance: \(value.distance(to: 1))")
+// 1 - 10 = -9
+
+print("1.advanced(by: -1) \(1.advanced(by: -1))")
+
+print("1.advanced(by: 1) \(1.advanced(by: 1))")
